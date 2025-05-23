9c4de7b489fc70272a993fb9a6d69a59d0b867c7
Adler Castro <adlercastro01@gmail.com>
2025-05-23 01:41:45 -0300

    :lipstick: style: diminuindo a opacidade do botão quando desativado

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,11 +10,11 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'rounded-4xl bg-purple-600 font-normal text-white transition-all hover:bg-purple-700 focus-visible:outline-white active:bg-purple-800 disabled:bg-purple-600/60',
+          'rounded-4xl bg-purple-600 font-normal text-white transition-all hover:bg-purple-700 focus-visible:outline-white active:bg-purple-800 disabled:bg-purple-600/50',
         loading:
           'rounded-4xl w-full bg-purple-600 font-normal text-white transition-all hover:bg-purple-700 focus-visible:outline-white active:bg-purple-800 disabled:bg-purple-600/60',
         outline:
-          'rounded-4xl bg-white font-normal text-purple-700 transition-all hover:bg-[#e6e6e6] hover:outline-3 focus-visible:outline-purple-700 active:bg-[#bfbfbf] disabled:bg-purple-600/60',
+          'rounded-4xl bg-white font-normal text-purple-700 transition-all hover:bg-[#e6e6e6] hover:outline-3 focus-visible:outline-purple-700 active:bg-[#bfbfbf] disabled:bg-purple-600/50',
         link: 'text-primary underline-offset-4 hover:underline',
         comeBack:
           'group rounded-full self-start text-white/60 ring-2 ring-white/60 transition-all hover:ring-white focus-visible:ring-white focus-visible:outline-offset-4 focus-visible:outline-purple-600 active:ring-white/80',
