0c1ba057ccc339f228d1a6e316990ed5ff1954f8
Adler Castro <adlercastro01@gmail.com>
2025-05-23 03:19:07 -0300

    :mag: debug do build

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,16 +1,21 @@
 import z from 'zod';
 
+console.log('🔍 API_URL:', process.env.API_URL);
+console.log('🔍 NEXT_PUBLIC_API_MID_URL:', process.env.NEXT_PUBLIC_API_MID_URL);
+console.log('🔍 NODE_ENV:', process.env.NODE_ENV);
+
 const envSchema = z.object({
-  API_URL: z.string().url().default('3333'),
-  NEXT_PUBLIC_API_MID_URL: z.string().url().default('http://localhost:3000'),
+  API_URL: z.string().url({ message: 'API_URL inválida' }),
+  NEXT_PUBLIC_API_MID_URL: z.string().url({ message: 'MID URL inválida' }),
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
 });
+
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-  console.error('Invalid environment variables:', _env.error.format());
+  console.error('❌ Invalid environment variables:', _env.error.format());
   throw new Error('Invalid environment variables');
 }
 
