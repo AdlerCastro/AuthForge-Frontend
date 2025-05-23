da6ac896fd551452fbc4e7bc70bdc03dbd0af1d1
Adler Castro <adlercastro01@gmail.com>
2025-05-22 23:48:15 -0300

    :wrench: chore: conexão com a api usando o axios

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,7 +1,9 @@
+'server-only';
+
 import { env } from '@/config/env.config';
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: env.DATABASE_URL,
+  baseURL: env.API_URL,
   withCredentials: true,
 });
