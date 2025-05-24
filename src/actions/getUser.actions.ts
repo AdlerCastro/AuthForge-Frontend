ad1d3016b39b32482c0ce9943e0e27778b7aa9fb
Adler Castro <adlercastro01@gmail.com>
2025-05-24 19:17:28 -0300

    :sparkles: feat: função para resgatar um único usuário

---
+++ src/actions/getUser.actions.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { UserResponse } from '@/types/responseUser.type';
+import { api } from '@/service/api.service';
+
+export async function getUser(id: string): Promise<UserResponse> {
+  try {
+    const token = (await cookies()).get('access_token')?.value;
+
+    if (!token) {
+      return {
+        success: false,
+        message: 'Token de autenticação não encontrado',
+        data: null,
+      };
+    }
+
+    const res = await api.get(`/users/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = res.data;
+
+    return {
+      success: data.success,
+      message: data.message || 'Usuário obtido com sucesso',
+      data: data,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: `Erro ao buscar o usuário. ${String(error)}`,
+      data: null,
+    };
+  }
+}
