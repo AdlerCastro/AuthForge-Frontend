28511c7703e69230aecfc077710c33475219f1e5
Adler Castro <adlercastro01@gmail.com>
2025-05-24 19:18:09 -0300

    :sparkles: feat: função para editar um usuário

---
+++ src/actions/editUser.actions.ts
@@ -0,0 +1,48 @@
+'use server';
+
+import { editUserSchema, EditUserSchemaType } from '@/schemas/editUser.schema';
+import { api } from '@/service/api.service';
+import { ResponseType } from '@/types/response.type';
+import { cookies } from 'next/headers';
+
+export async function editUser(
+  id: string,
+  data: EditUserSchemaType,
+): Promise<ResponseType> {
+  try {
+    const parsedData = editUserSchema.parse(data);
+
+    //eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { confirmPassword, ...rest } = parsedData;
+
+    const safePayload = {
+      ...rest,
+      birth_date: new Date(rest.birth_date).toISOString(),
+    };
+
+    const token = (await cookies()).get('access_token')?.value;
+
+    const response = await api.patch(`/admin/${id}`, safePayload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.status === 200) {
+      return {
+        success: true,
+        message: response.data.message || 'Usuário editado com sucesso',
+      };
+    }
+
+    return {
+      success: false,
+      message: `Erro ao editar o usuário. ${response.data.message}`,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: `Erro ao editar o usuário. ${String(error)}`,
+    };
+  }
+}
