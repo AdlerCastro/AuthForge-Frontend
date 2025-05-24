092ca769aaa1a5dcd645e353d93853ed19ed374a
Adler Castro <adlercastro01@gmail.com>
2025-05-24 15:48:38 -0300

    :sparkles: feat: header

---
+++ src/components/molecules/header.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/lib/utils';
+import { Link } from '../atoms/link';
+import { Pages } from '@/enum/pages.enum';
+import { ButtonProfile } from '../atoms/buttonProfile';
+
+export function Header({
+  className,
+  ...props
+}: React.HtmlHTMLAttributes<HTMLDivElement>) {
+  return (
+    <header
+      className={cn(
+        'bg-primary-100 shadow-header fixed top-0 z-10 flex h-20 w-full items-center justify-center bg-white px-10 py-4',
+        className,
+      )}
+      {...props}
+    >
+      <div className='flex w-full max-w-[1280px] flex-row items-center justify-between'>
+        <h1 className='animate-pulse text-3xl font-bold text-nowrap text-black'>
+          AuthForge
+        </h1>
+        <nav className='fixed right-1/2 left-1/2 flex items-center justify-center gap-5'>
+          <Link href={Pages.USERS}>Usuários</Link>
+          <Link href={Pages.ABOUT}>Sobre</Link>
+        </nav>
+        <ButtonProfile />
+      </div>
+    </header>
+  );
+}
