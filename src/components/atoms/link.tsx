d3d25bea3fc5634714a11d32d4d9445ff3ed5932
Adler Castro <adlercastro01@gmail.com>
2025-05-24 21:46:12 -0300

    :lipstick: style: diminuindo a duração das animações

--- src/components/atoms/link.tsx
+++ src/components/atoms/link.tsx
@@ -3,12 +3,12 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, VariantProps } from 'class-variance-authority';
 
 const linkVariants = cva(
-  'inline-flex font-inter text-base items-center disabled:cursor-none justify-center gap-2 whitespace-nowrap outline-0 focus-visible:underline focus-visible:underline-offset-4 transition-all',
+  'inline-flex font-inter text-base items-center disabled:cursor-none justify-center gap-2 whitespace-nowrap outline-0 focus-visible:underline focus-visible:underline-offset-4 transition-all duration-100',
   {
     variants: {
       variant: {
         default:
-          'duration-300 hover:text-purple-600 focus-visible:text-purple-600 active:text-purple-800 hover:underline hover:underline-offset-2',
+          'hover:text-purple-600 focus-visible:text-purple-600 active:text-purple-800 hover:underline hover:underline-offset-2',
       },
       size: {
         default: 'px-5 py-2',
