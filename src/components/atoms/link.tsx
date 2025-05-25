a7c7762cb322be975659f5c1ef4b6fe3939e47e8
Adler Castro <adlercastro01@gmail.com>
2025-05-24 22:40:24 -0300

    :lipstick: feat: criando variantes para utilizar com ícones

--- src/components/atoms/link.tsx
+++ src/components/atoms/link.tsx
@@ -9,9 +9,11 @@ const linkVariants = cva(
       variant: {
         default:
           'hover:text-purple-600 focus-visible:text-purple-600 active:text-purple-800 hover:underline hover:underline-offset-2',
+        icon: 'rounded-4xl bg-white font-normal text-purple-700 transition-all hover:bg-[#e6e6e6] hover:text-purple-700 hover:no-underline hover:outline-3 focus-visible:no-underline focus-visible:outline-purple-700 active:bg-[#bfbfbf] active:text-purple-700 disabled:bg-purple-600/50',
       },
       size: {
         default: 'px-5 py-2',
+        icon: 'size-9',
       },
     },
     defaultVariants: {
